436e395a903620280a3cda1a488fd5d92823f658
Carlos Hernandez <carlos.hernandez-ext@dominion-ext.com>
2021-08-21 07:02:55 -0600

    scripts base de datos

---
+++ estudiante.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `estudiante` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
+  `Edad` int(11) NOT NULL,
+  `Grado` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4
